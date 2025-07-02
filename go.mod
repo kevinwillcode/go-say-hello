5d0c58e9f445649df36930ab2d92bd3619003d23
kevinwillcode <kevinity310@outlook.com>
2025-07-02 20:16:00 +0700

    major change, add parameter to say_hello function

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/kevinwillcode/go-say-hello
+module github.com/kevinwillcode/go-say-hello/v2
 
 go 1.24.3
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,9 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World v1.5.0"
+func SayHello(name string) string {
+	return "Hello " + name + " v2.0.0"
 }
+
+//func SayHello() string {
+//	return "Hello World v1.5.0"
+//}
